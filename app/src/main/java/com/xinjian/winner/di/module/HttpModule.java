c69b159c0fcfc8b8b9f437882224671f66d899cb
lixinjian <lixinjian00>
2017-05-23 23:34:19 +0800

    20170523晚

--- app/src/main/java/com/xinjian/winner/di/module/HttpModule.java
+++ app/src/main/java/com/xinjian/winner/di/module/HttpModule.java
@@ -1,10 +1,16 @@
 package com.xinjian.winner.di.module;
 
+import com.xinjian.winner.di.qualifier.ZhihuUrl;
+import com.xinjian.winner.model.http.api.ZhihuApis;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Administrator on 2017/5/19 0019.
@@ -19,6 +25,27 @@ public class HttpModule {
         return new Retrofit.Builder();
     }
 
+    @Singleton
+    @Provides
+    OkHttpClient.Builder provideOkHttpBuilder() {
+        return new OkHttpClient.Builder();
+    }
+
+    @Singleton
+    @Provides
+    @ZhihuUrl
+    Retrofit provideZhihuRetrofit(Retrofit.Builder builder, OkHttpClient client) {
+        return createRetrofit(builder, client, ZhihuApis.HOST);
+    }
 
 
+
+    private Retrofit createRetrofit(Retrofit.Builder builder, OkHttpClient client, String url) {
+        return builder
+                .baseUrl(url)
+                .client(client)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
 }
